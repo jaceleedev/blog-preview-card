719ac09c47a14ff667262c1a20a575156bfe66b2
jaceleedev <jaceleedev@gmail.com>
2024-07-16 14:22:58 +0900

    feat: :sparkles: setup initial BlogPreviewCard component with styling

---
+++ src/components/blog-preview-card/BlogPreviewCard.tsx
@@ -0,0 +1,9 @@
+import styles from './BlogPreviewCard.module.css';
+
+function BlogPreviewCard() {
+  return (
+    <article className={styles['blog-preview-card']}>BlogPreviewCard</article>
+  );
+}
+
+export default BlogPreviewCard;
