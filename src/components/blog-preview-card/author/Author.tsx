cbeaa4a066d53c8284df6a13e5f7390d0acd867e
jaceleedev <jaceleedev@gmail.com>
2024-07-16 14:00:53 +0900

    feat: :sparkles: add Author component with styling

---
+++ src/components/blog-preview-card/author/Author.tsx
@@ -0,0 +1,24 @@
+import styles from './Author.module.css';
+
+type AuthorProps = {
+  name: string;
+  imagePath: string;
+};
+
+function Author({ name, imagePath }: AuthorProps) {
+  return (
+    <div className={styles.author}>
+      <div
+        className={styles.author__image}
+        style={{
+          backgroundImage: `url(${imagePath})`,
+          backgroundColor: 'lightgray',
+        }}
+        aria-label={`profile picture of ${name}`}
+      />
+      <p className={styles.author__name}>{name}</p>
+    </div>
+  );
+}
+
+export default Author;
