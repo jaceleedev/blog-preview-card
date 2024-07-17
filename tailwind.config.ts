028ab82fcde868ddd5f7c9ec2fcb80b25f56ee7c
jaceleedev <jaceleedev@gmail.com>
2024-07-17 14:18:30 +0900

    feat: :wrench: add typography and utility classes to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: [
@@ -20,8 +21,84 @@ const config: Config = {
       fontFamily: {
         figTree: ['var(--font-figTree)'],
       },
+      fontSize: {
+        'body-s-regular': '0.875rem', // 14px -> 0.875rem
+        'body-s-bold': '0.875rem', // 14px -> 0.875rem
+        'body-m': '1rem', // 16px -> 1rem
+        heading: '1.5rem', // 24px -> 1.5rem
+        'body-s-regular-mobile': '0.75rem', // 12px -> 0.75rem
+        'body-s-bold-mobile': '0.75rem', // 12px -> 0.75rem
+        'body-m-mobile': '0.875rem', // 14px -> 0.875rem
+        'heading-mobile': '1.25rem', // 20px -> 1.25rem
+      },
+      lineHeight: {
+        'body-s': '150%',
+        'body-m': '150%',
+        heading: '150%',
+      },
+      fontWeight: {
+        regular: '500',
+        bold: '800',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities, theme }) {
+      const newUtilities = {
+        '.body-s-regular': {
+          fontFamily: theme('fontFamily.figTree') || 'var(--font-figTree)',
+          fontSize: theme('fontSize.body-s-regular') || '0.875rem',
+          fontWeight: theme('fontWeight.regular') || '500',
+          lineHeight: theme('lineHeight.body-s') || '150%',
+          color: theme('colors.charcoal-black') || '#111111',
+        },
+        '.body-s-bold': {
+          fontFamily: theme('fontFamily.figTree') || 'var(--font-figTree)',
+          fontSize: theme('fontSize.body-s-bold') || '0.875rem',
+          fontWeight: theme('fontWeight.bold') || '800',
+          lineHeight: theme('lineHeight.body-s') || '150%',
+          color: theme('colors.charcoal-black') || '#111111',
+        },
+        '.body-s-bold-fixed': {
+          fontFamily: theme('fontFamily.figTree') || 'var(--font-figTree)',
+          fontSize: theme('fontSize.body-s-bold') || '0.875rem',
+          fontWeight: theme('fontWeight.bold') || '800',
+          lineHeight: theme('lineHeight.body-s') || '150%',
+          color: theme('colors.charcoal-black') || '#111111',
+        },
+        '.body-m': {
+          fontFamily: theme('fontFamily.figTree') || 'var(--font-figTree)',
+          fontSize: theme('fontSize.body-m') || '1rem',
+          fontWeight: theme('fontWeight.regular') || '500',
+          lineHeight: theme('lineHeight.body-m') || '150%',
+          color: theme('colors.medium-gray') || '#7F7F7F',
+        },
+        '.heading': {
+          fontFamily: theme('fontFamily.figTree') || 'var(--font-figTree)',
+          fontSize: theme('fontSize.heading') || '1.5rem',
+          fontWeight: theme('fontWeight.bold') || '800',
+          lineHeight: theme('lineHeight.heading') || '150%',
+          color: theme('colors.charcoal-black') || '#111111',
+        },
+        '@screen mobile': {
+          '.body-s-regular': {
+            fontSize: theme('fontSize.body-s-regular-mobile') || '0.75rem',
+          },
+          '.body-s-bold': {
+            fontSize: theme('fontSize.body-s-bold-mobile') || '0.75rem',
+          },
+          '.body-m': {
+            fontSize: theme('fontSize.body-m-mobile') || '0.875rem',
+          },
+          '.heading': {
+            fontSize: theme('fontSize.heading-mobile') || '1.25rem',
+          },
+        },
+      };
+
+      addUtilities(newUtilities);
+    }),
+  ],
 };
+
 export default config;
